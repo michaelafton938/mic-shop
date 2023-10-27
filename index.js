8e329293c4ebc5de759efaba2070c0af7c258562
Alexis <110925334+michaelafton938@users.noreply.github.com>
2023-10-27 13:32:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!'; // Préfix de commande personnalisable
+
+// Commande Pour ajouter un nouvel objet sur le marché
+client.on('me')
